4867713757ed663e25b0a12d8e40182b480a7730
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-05-25 10:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+  // Get the reference to the colorSelect dropdown
+  const dropdown = document.getElementById("colorSelect");
+
+  // Get the selected index of the dropdown
+  const selectedIndex = dropdown.selectedIndex;
+
+  // Remove the selected color option from the dropdown
+  dropdown.remove(selectedIndex);
+}
